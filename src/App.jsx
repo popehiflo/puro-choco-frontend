ba0a430fc26af89b0fdd5486930a6475bb7e46f8
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-27 18:57:35 -0500

    feat: fuentes, icons, test package para slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
-import './styles/App.css';
-
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+  <>
+    <header className="header">
+      <nav className="navbar">Header</nav>
     </header>
-  </div>
+    <section className="main" />
+  </>
 );
 
 export default App;
